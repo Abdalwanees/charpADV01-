8077fcfd98d5910be5cf73aafcda0dab2d04ba34
Abdalwanees <gabdo7682@gmail.com>
2024-07-26 16:27:07 +0300

    Add project files.

---
+++ C42-G01-ADV01-/BubbleSort/Sort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace C42_G01_ADV01.BubbleSort
+{
+    internal class Sort
+    {
+        public static void BubbleSort(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                Console.WriteLine("Array is null or empty.");
+                return;
+            }
+            bool swapped;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                Console.WriteLine($"\nStep {i + 1}:");
+                foreach (int num in array)
+                {
+                    Console.Write(num + " ");
+                }
+
+                // If no two elements were swapped by inner loop, then break
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ C42-G01-ADV01-/Program.cs
@@ -0,0 +1,31 @@
+﻿using C42_G01_ADV01.BubbleSort;
+using C42_G01_ADV01.Genaric;
+
+namespace C42_G01_ADV01_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            int[] arr = { 3, 5, 9, 7, 5, 6, 5, 1, 4, 2, 3 };
+
+            Console.Write("Arr :[");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]},");
+            }
+            Console.WriteLine("\n---------------");
+            Console.WriteLine("Array After Sorting");
+            Sort.BubbleSort(arr);
+            #endregion
+            Console.WriteLine();
+            #region MyRegion
+            Range<int> range = new Range<int>(5, 15);
+            Console.WriteLine($"Is 10 in range: {range.IsInRange(10)}");  // True
+            Console.WriteLine($"Is 20 in range: {range.IsInRange(20)}");  // False
+            Console.WriteLine($"Length of range: {range.Length()}");      // 10
+            #endregion
+        }
+    }
+}
